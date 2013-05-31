4ca78d0a0b1ecc53096bbe3f55c4adb6c93f44d3
Peter Silver <xoxo@crystalise.tw>
2013-05-31 14:05:55 +0200

    upload controller

---
+++ Mvc_fileUploader/Controllers/UploadedController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_fileUploader.Models;
+
+namespace Mvc_fileUploader.Controllers
+{
+    public class UploadedController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("UploadedFiles");
+        }
+        
+        
+        [HttpGet]
+        public ActionResult UploadedFiles()
+        {
+            var uploadedFiles = new List<UploadedFile>();
+            
+            var files = Directory.GetFiles(Server.MapPath("~/fileUploads/"));
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+
+                var uploadedFile = new UploadedFile();
+                
+                uploadedFile.Name = Path.GetFileName(file);
+                uploadedFile.Size = fileInfo.Length;
+                uploadedFile.Path = ("~/fileUploads/") + Path.GetFileName(file);
+                
+                uploadedFiles.Add(uploadedFile);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ Mvc_fileUploader/Models/UploadedFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_fileUploader.Models
+{
+    public class UploadedFile
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        
+        public long Size { get; set; }
+        
+    }
+}
